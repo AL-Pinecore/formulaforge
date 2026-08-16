ac14b8a435834eaf461b0632498a291596501f8c
James Wang <145985058+AL-Pinecore@users.noreply.github.com>
2026-08-16 23:05:10 +0800

    chore: alias for root

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,21 @@
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vitest/config'
 import { defineVitestProject } from '@nuxt/test-utils/config'
+
+const resolve = (path: string) => fileURLToPath(new URL(path, import.meta.url))
 
 export default defineConfig({
   test: {
     projects: [
       {
+        resolve: {
+          alias: {
+            '~': resolve('./app'),
+            '@': resolve('./app'),
+            '~~': resolve('.'),
+            '@@': resolve('.'),
+          },
+        },
         test: {
           name: 'unit',
           include: ['test/unit/*.{test,spec}.ts'],
